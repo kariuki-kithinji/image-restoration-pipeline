22aa6440970c1ee67e829dcab85f7bf3daa1f711
kariuki-kithinji <121386121+kariuki-kithinji@users.noreply.github.com>
2023-06-30 13:59:06 +0300

    fixed the upsampler bug

--- upscale/__init__.py
+++ upscale/__init__.py
@@ -10,7 +10,7 @@ import cv2
 class BGUpsampler:
     def __init__(self):
         self.model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        self.bg_upsampler = RealESRGANer(
+        self.upsampler = RealESRGANer(
                 scale=2, 
                 model_path='https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth',
                 model=self.model,tile=400,tile_pad=10,pre_pad=0,half=True)  # need to set False in CPU mode
@@ -19,7 +19,7 @@ class FaceUpsampler:
     def __init__(self):
         self.arch = 'clean'
         self.channel_multiplier = 2
-        self.bg_upsampler = BGUpsampler()
+        self.bg_upsampler = BGUpsampler().upsampler
         self.model_path = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
         self.restorer = GFPGANer(
                 model_path=self.model_path,upscale=2,arch=self.arch,
